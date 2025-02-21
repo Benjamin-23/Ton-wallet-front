2919e567c3e77c0156d28bec386aa06b6fbd9a4f
benjamin-23 <kitongabenja34@gmail.com>
2025-02-21 19:49:50 +0300

    Nft lending screen

--- src/pages/Nft/index.tsx
+++ src/pages/Nft/index.tsx
@@ -1,7 +1,7 @@
 import NFTLendingComponent from "@/components/NftLending";
 export default function NFTLending() {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
+    <div className="">
       <NFTLendingComponent />
     </div>
   );
